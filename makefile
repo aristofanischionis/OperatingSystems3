ee44dd7cee72aa8531fcc474430ca123caa598b3
aristofanischionis <aristofanischionis@gmail.com>
2018-12-08 14:41:21 +0200

    working

--- makefile
+++ makefile
@@ -1,23 +1,25 @@
 #Got this from the official Website of Alexis Delis
 OBJS    = myport.o vessel.o monitor.o port-master.o
+HEADERS = vessel.h
 OUT  	= myport vessel monitor port-master
 CC		= gcc -g
+LIBS    += -lpthread -lrt
 # -g option enables debugging mode 
 # make the executables
 
 all : $(OUT)
 
 myport : myport.o vessel.h
-	$(CC) -o myport myport.c
+	$(CC) -o myport myport.c $(LIBS) $(HEADERS)
 
 vessel : vessel.o vessel.h
-	$(CC) -o vessel vessel.c
+	$(CC) -o vessel vessel.c $(LIBS) $(HEADERS)
 
 monitor : monitor.o
-	$(CC) -o monitor monitor.c
+	$(CC) -o monitor monitor.c $(LIBS) $(HEADERS)
 
 port-master : port-master.o
-	$(CC) -o port-master port-master.c
+	$(CC) -o port-master port-master.c $(LIBS) $(HEADERS)
 
 # clean house
 clean:
--- myport.c
+++ myport.c
@@ -28,7 +28,7 @@ struct SharedMemory {
     sem_t exitSM;
     // types
     configfile conf;
-    //
+    // //
     char* publicLedger;
     char* logfile;
     VesselInfo ship;
@@ -38,95 +38,88 @@ typedef struct SharedMemory SharedMemory;
 
 int main(int argc, char *argv[])
 {
-	int retval;
     int id=0, err=0;
-    sem_t *entranceSM, *exitSM, *sp;
-    char *conFile, *s1, *s2;
-    FILE *fp;
-    char buff[50];
+    sem_t entranceSM, exitSM, *sp;
+    char conFile[15], s1[10], s2[5];
+    FILE *fp, *fp1, *fp2;
+    char buff[15];
+    // 
+    // s1 = malloc(10 * sizeof(char));
+    // s2 = malloc(5 * sizeof(char));
+    // conFile = malloc(15 * sizeof(char));
+    // buff = malloc(15 * sizeof(char));
     // make struct configfile
     configfile *struct_configfile = malloc(sizeof(configfile));
     if (struct_configfile == NULL) {printf("Couldn't malloc struct config file"); return 1; }
     //
-    if(argc != 3){ printf("You haven't supplied enough arguments"); return 1; }
+    printf("BROMAAAAAAAAAAAAAAAAAAA");
+    if(argc != 3){ printf("You haven't supplied enough arguments"); return 2; }
     if(!strcmp(argv[1], "-l")){
         strcpy(conFile, argv[2]);
     }
-    else { printf("You haven't supplied -l parameter"); return 1; }
+    else { printf("You haven't supplied -l parameter"); return 3; }
     // read from file
     fp = fopen(conFile, "r");
-    while (fgets(buff, sizeof buff, fp) != NULL) {
-        // process line here
-        if (sscanf(buff, "%s %s", s1, s2) == 2) {
-        // there weren 2 items to convert
-            if(!strcmp(s1,"type1")){
-                struct_configfile->type1 = s2[0];
-                continue;
-            }
-            if(!strcmp(s1,"type2")){
-                struct_configfile->type2 = s2[0];
-                continue;
-            }
-            if(!strcmp(s1,"type3")){
-                struct_configfile->type3 = s2[0];
-                continue;
-            }
-            if(!strcmp(s1,"capacity1")){
-                struct_configfile->ca1 = atoi(s2);
-                continue;
-            }
-            if(!strcmp(s1,"capacity2")){
-                struct_configfile->ca2 = atoi(s2);
-                continue;
-            }
-            if(!strcmp(s1,"capacity3")){
-                struct_configfile->ca3 = atoi(s2);
-                continue;
-            }
-            if(!strcmp(s1,"cost1")){
-                struct_configfile->co1 = atoi(s2);
-                continue;
-            }
-            if(!strcmp(s1,"cost2")){
-                struct_configfile->co2 = atoi(s2);
-                continue;
-            }
-            if(!strcmp(s1,"cost3")){
-                struct_configfile->co3 = atoi(s2);
-                continue;
-            }
-            else {
-                printf("I read something weird %s, %s \n", s1,s2);
-                return 1;
-            }
+    
+    while (fscanf(fp,"%s %s", s1, s2) == 2) {
+        if(!strcmp(s1,"type1")){
+            struct_configfile->type1 = s2[0];
+            continue;
+        }
+        if(!strcmp(s1,"type2")){
+            struct_configfile->type2 = s2[0];
+            continue;
+        }
+        if(!strcmp(s1,"type3")){
+            struct_configfile->type3 = s2[0];
+            continue;
+        }
+        if(!strcmp(s1,"capacity1")){
+            struct_configfile->ca1 = atoi(s2);
+            continue;
+        }
+        if(!strcmp(s1,"capacity2")){
+            struct_configfile->ca2 = atoi(s2);
+            continue;
+        }
+        if(!strcmp(s1,"capacity3")){
+            struct_configfile->ca3 = atoi(s2);
+            continue;
+        }
+        if(!strcmp(s1,"cost1")){
+            struct_configfile->co1 = atoi(s2);
+            continue;
+        }
+        if(!strcmp(s1,"cost2")){
+            struct_configfile->co2 = atoi(s2);
+            continue;
+        }
+        if(!strcmp(s1,"cost3")){
+            struct_configfile->co3 = atoi(s2);
+            continue;
         }
         else {
-            printf("I read something weird %s \n", s1);
-            return 1;
+            printf("I read something weird %s, %s \n", s1,s2);
+            return 4;
         }
     }
-    fclose(fp);
+    
     // make public ledger file
-    fp = fopen("/publicLedger", "a");
-    fclose(fp);
+    fp1 = fopen("publicLedger", "a");
     // make log file
-    fp = fopen("/log", "a");
-    fclose(fp);
+    fp2 = fopen("log", "a");
     // make shared memory
     id = shmget(IPC_PRIVATE ,sizeof(SharedMemory) ,0666); /*  Make  shared  memory  segment  */
-    if (id ==  (void *) -1) {perror ("Creation"); exit(2);}
+    if (id ==  (void *) -1) {perror ("Creation"); exit(6);}
     else printf("Allocated. %d\n", (int)id);
     // attach to sp
-    sp = (sem_t  *)shmat(id ,(void*) 0, 0);
-    if ( sp == (void *)  -1) { perror("Attachment."); exit (2);}
+    sp = (sem_t*)shmat(id ,(void*) 0, 0);
+    if ( sp == (void *)  -1) { perror("Attachment."); exit (7);}
 
     /*  Initialize  the  semaphores. */
-    retval = sem_init(sp ,1,2);
-    if (retval != 0) { perror("Couldn ’t initialize. sp"); exit(3); }
-    retval = sem_init(entranceSM ,1,2);
-    if (retval != 0) { perror("Couldn ’t initialize. entranceSM"); exit(3); }
-    retval = sem_init(exitSM ,1,2);
-    if (retval != 0) { perror("Couldn ’t initialize. exitSM"); exit(3); }
+    if (sem_init(sp ,1,2) != 0) { perror("Couldn ’t initialize. sp"); exit(8); }
+    if (sem_init(&entranceSM ,1,3) != 0) { perror("Couldn ’t initialize. entranceSM"); exit(9); }
+    if (sem_init(&exitSM ,1,4) != 0) { perror("Couldn ’t initialize. exitSM"); exit(10); }
     // exec all programs
     // setsid sh -c 'exec command <> /dev/tty2 >&0 2>&1' to exec in a new tty
     // make sure you fork and exec your childern and then wait for them to finish
@@ -137,12 +130,20 @@ int main(int argc, char *argv[])
     free(struct_configfile);
     // destroy sms
     sem_destroy(sp);
-    sem_destroy(entranceSM);
-    sem_destroy(exitSM);
+    sem_destroy(&entranceSM);
+    sem_destroy(&exitSM);
     // delete shm seg
     err = shmctl(id, IPC_RMID , 0); /*  Remove  segment  */
     if(err ==  -1) perror("Removal.");
     else printf("Removed. %d\n", (int)(err));
+    fclose(fp);
+    fclose(fp1);
+    fclose(fp2);
+    // free strings
+    // free(s1);
+    // free(s2);
+    // free(conFile);
+    // free(buff);
     
     return  0;
 }
